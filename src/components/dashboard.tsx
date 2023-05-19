0f0e7d88fc964144930aeeb66a80e30dbe36bf20
kevywilly <kevywilly@gmail.com>
2023-05-19 08:59:07 -0400

    cleanup layout for iphone

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -9,7 +9,7 @@ const DriveButton = (props: {command: string, onClick: (command: string) => void
     return (
         <button
             onClick={() => onClick(command)}
-            className="rounded-md bg-blue-400 p-2"
+            className="rounded-md bg-blue-400 p-2 w-full"
         >{command}
         </button>
     )
@@ -23,7 +23,7 @@ const CategoryButton = (props: {category: CategoryCount, onClick: (category: Cat
             <div className="flex flex-col w-full gap-4 justify-center">
                 <button
                     onClick={() => onClick(category)}
-                    className="rounded-md bg-blue-400 w-full p-4"
+                    className="rounded-full bg-black w-full p-2 text-white"
                 >{category.name}</button>
                 <div className="text-center font-bold text-xl">{category.entries}</div>
             </div>
@@ -86,30 +86,38 @@ export default function Dashboard() {
             <div className="flex flex-col xl:flex-row  w-full gap-4 items-center">
                 <div className="flex flex-col w-full xl:w-1/2 justify-center items-center gap-2">
                 <img
-                    className="aspect-square w-4/5 rounded-xl"
+                    className="aspect-square w-full rounded-xl"
                     src={api.routes.stream_url}
                     alt="Jetson Rover Stream"
                     content="multipart/x-mixed-replace; boundary=frame"
                 />
                 </div>
                 <div className="grid grid-cols-4 gap-4">
-                    <button onClick={handleAutoDrive}
-                            className={`rounded-md ${autodrive ? "bg-green-500" : "bg-black"} text-white p-2`}>
-                        Auto {autodrive ? "OFF" : "ON"}
-                    </button>
-                    <button onClick={speedDown} className="rounded-md bg-red-400 p-2 w-full">-</button>
-                    <button className="rounded-md bg-green-100 p-2 w-full font-bold">{speed}</button>
-                    <button onClick={speedUp} className="rounded-md bg-green-400 p-2 w-full">+</button>
+                    <div className="col-span-3 w-full">
+                        <DriveButton command="forward" onClick={handleCmdClick}/>
+                    </div>
+                    <div className="col-span-1 w-full">
+                        <button onClick={speedUp} className="rounded-full bg-green-400 p-2 w-full">+</button>
+                    </div>
+
                     <DriveButton command="left" onClick={handleCmdClick}/>
-                    <DriveButton command="forward" onClick={handleCmdClick}/>
-                    <DriveButton command="backward" onClick={handleCmdClick}/>
-                    <DriveButton command="right" onClick={handleCmdClick}/>
                     <DriveButton command="stop" onClick={handleCmdClick}/>
+                    <DriveButton command="right" onClick={handleCmdClick}/>
+                    <button onClick={speedDown} className="rounded-full bg-red-400 p-2 w-full">-</button>
+
+                    <div className="col-span-3 w-full">
+                        <DriveButton command="backward" onClick={handleCmdClick}/>
+                    </div>
+                    <button className="rounded-full bg-gray-200 p-2 w-full font-bold">{speed}</button>
 
                     { categories && categories.map((k) => (
                         <CategoryButton category={k} onClick={handleCategoryClick}/>
                     ))
                     }
+                    <button onClick={handleAutoDrive}
+                            className={`rounded-md ${autodrive ? "bg-green-500 text-white" : "bg-gray-100 text-black"}  p-2 w-full`}>
+                        Auto {autodrive ? "OFF" : "ON"}
+                    </button>
                 </div>
 
 
