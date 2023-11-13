b256d9e6bf4ef5777a5a67f9100777e77036c45b
kevywilly <kevywilly@gmail.com>
2023-11-13 13:07:36 -0500

    cleanup

--- src/components/dashboard.tsx
+++ src/components/dashboard.tsx
@@ -4,9 +4,6 @@ import {useQuery, useQueryClient} from "react-query";
 import {Joystick} from 'react-joystick-component';
 import {IJoystickUpdateEvent} from "react-joystick-component/build/lib/Joystick";
 
-const DEFAULT_DRIVE_POWER = 0.32
-const DEFAULT_TURN_POWER = 0.16
-
 const CategoryButton = (props: {category: CategoryCount, onClick: (category: CategoryCount) => void}) => {
 
     const {category, onClick} = props
@@ -22,38 +19,7 @@ const CategoryButton = (props: {category: CategoryCount, onClick: (category: Cat
         )
 }
 
-const ControlPanel = (props : {onClick: (x: number, y: number, z: number, speed: number) => void}) => {
-    const {onClick} = props
-    return (
-        <div className="grid grid-cols-3 text-white font-bold gap-2 w-full">
-            <div className="col-span-full text-center">
-                <button className="control-button bg-green-800" onClick={() => onClick(1.0, 0.0, 0.0, DEFAULT_DRIVE_POWER)}>
-                    Forward
-                </button>
-            </div>
-            <div className="col-span-1">
-                <button className="control-button bg-orange-800" onClick={() => onClick(0.0, 0.0, 1.0, DEFAULT_TURN_POWER)}>
-                    Left
-                </button>
-            </div>
-            <div className="col-span-1">
-                <button className="control-button bg-red-800" onClick={() => onClick(0.0, 0.0, 0.0, 0.0)}>
-                    Stop
-                </button>
-            </div>
-            <div className="col-span-1">
-                <button className="control-button bg-orange-800" onClick={() => onClick(0.0, 0.0, -1.0, DEFAULT_TURN_POWER)}>
-                    Right
-                </button>
-            </div>
-            <div className="col-span-full">
-                <button className="control-button bg-yellow-800" onClick={() => onClick(-1.0, 0.0, 0.0, DEFAULT_DRIVE_POWER)}>
-                    Reverse
-                </button>
-            </div>
-        </div>
-    )
-}
+
 export default function Dashboard() {
 
     const queryClient = useQueryClient()
@@ -73,54 +39,29 @@ export default function Dashboard() {
         api.methods.drive(velocity).then(setVelocityResponse)
     },[velocity])
 
-    const not_zero = (a?: number, b?: number): boolean => (((a || 0) !== 0) && ((b || 0) !== 0))
-
     const handleAutodrive = () => {
         setAutodrive(false)
         api.methods.autodrive().then((e) => setAutodrive(e.status))
     }
 
-    const handleControlClick = (x: number, y: number, z: number, speed: number) => {
-
-        return 
-        if(not_zero(x, velocity.x) || not_zero(y, velocity.y) || not_zero(z,velocity.z)) {
-            setVelocity(VELOCITY_ZERO)
-            return
-        }
-
-        setVelocity({x: x*speed, y: y*speed, z: z*speed})
-
-        /*
-        let cat: string | undefined = undefined
-
-        if(capture) {
-            if(x > 0) {
-                cat = "forward"
-            } else if (z > 0) {
-                cat = "left"
-            } else if (z < 0) {
-                cat = "right"
-            }
-        }
-        if(cat) {
-            api.methods.collect_image(cat).then(() => queryClient.invalidateQueries("categories"))
-        }
-        */
-    }
-
     const handleJoy = (e: IJoystickUpdateEvent) => {
 
-        let y: number = e.x || 0
-        let x: number = e.y || 0
+        let x: number = (e.y || 0)*0.5
+        let y: number = -(e.x || 0)*0.5
         let z: number = 0
 
+        if(Math.abs(y) < .25) {
+            y = 0;
+        }
         setVelocity({x,y,z})
-        
     }
 
     const handleJoy2 = (e: IJoystickUpdateEvent) => {
+        const x = 0
+        const y = 0
+        const z = -(e.x || 0)*2.0
 
-        return
+        setVelocity({x,y,z})
 
         
     }
@@ -152,8 +93,8 @@ export default function Dashboard() {
             <div className="w-full absolute fixed bottom-6 z-100 flex flex-col gap-4 items-center justify-between">
 
                 <div className="flex flex-row gap-16">
-                <Joystick size={120} sticky={false} baseColor="white" stickColor="blue" move={handleJoy} stop={handleJoy} minDistance={5} />
-                <Joystick size={120} sticky={false} baseColor="white" stickColor="blue" move={handleJoy2} stop={handleJoy2} minDistance={5} />
+                <Joystick size={120} sticky={false} baseColor="white" stickColor="blue" move={handleJoy} stop={handleJoy} minDistance={20} />
+                <Joystick size={120} sticky={false} baseColor="white" stickColor="blue" move={handleJoy2} stop={handleJoy2} minDistance={20} />
                 </div>
                 <div className="text-white flex flex-row justify-center gap-4 items-center text-xs">
                     <button className={`toggle-button ${autodrive ? "bg-green-500" : "bg-red-600"}`} onClick={handleAutodrive}>
